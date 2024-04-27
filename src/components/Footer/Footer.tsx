c889062fbc2de8aa71d671452446a74f3819f63c
Mary Salvatore <marysalvatore084@gmail.com>
2024-04-27 19:12:23 -0400

    footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import {Row, Col} from 'react-bootstrap';
 
 const Footer = () => {
   return (
-          <Row style={{margin: '5em', background: 'white'}} >
+          <Row className='bg-white' style={{margin: '5em'}} >
             <Col xs={12} md={{span: 8, offset: 2}} className='flex align-center justify-center'>
             <p>Copyright © Defi-Layer</p>
             </Col>
